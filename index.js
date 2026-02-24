1101c1972ddab6524303cfd796284095420c06c4
Alexander Timonin <2sdaiwxet@gmail.com>
2026-02-24 18:54:19 +0500

    done

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
+const board = [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]];
+let turn = CROSS;
+
+let victory = false;
+
 startGame();
 addResetListener();
 
@@ -26,14 +31,80 @@ function renderGrid (dimension) {
     }
 }
 
+function checkVictory(sign) {
+    for (let row = 0; row < 3; row++) {
+        let all = true;
+        for (let col = 0; col < 3; col++){
+            if (board[row][col] !== sign) all = false;
+        }
+        if (all) return true;
+    }
+
+    for (let col = 0; col < 3; col++){
+        let all = true;
+        for (let row = 0; row < 3; row++) {
+            if (board[row][col] !== sign) all = false;
+        }
+        if (all) return true;
+    }
+
+    let allDiag1 = true;
+    for (let i = 0; i < 3; i++) {
+        if (board[i][i] !== sign) allDiag1 = false;
+    }
+    if (allDiag1) return true;
+
+    let allDiag2 = true;
+    for (let i = 0; i < 3; i++) {
+        if (board[2 - i][i] !== sign) allDiag2 = false;
+    }
+    if (allDiag2) return true;
+}
+
+function outOfSteps() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] === EMPTY) return false;
+        }
+    }
+    return true;
+}
+
 function cellClickHandler (row, col) {
-    // Пиши код тут
     console.log(`Clicked on cell: ${row}, ${col}`);
+    if (victory) return;
+    if (board[row][col] !== EMPTY) return;
+    board[row][col] = turn;
 
+    renderSymbolInCell(turn, row, col);
 
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+    if (outOfSteps()) {
+        alert("Победила дружба");
+        return;
+    }
+
+    if (checkVictory(CROSS)){
+        colorCells(CROSS);
+        alert("Победили крестики");
+        victory = true;
+    }
+    if (checkVictory(ZERO)){
+        colorCells(ZERO);
+        alert("Победили нолики");
+        victory = true;
+    }
+
+    turn = turn === CROSS ? ZERO : CROSS;
+}
+
+function colorCells(sign) {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] !== sign) continue;
+            let cell = findCell(i, j);
+            cell.style.color = "red";
+        }
+    }
 }
 
 function renderSymbolInCell (symbol, row, col, color = '#333') {
@@ -55,6 +126,15 @@ function addResetListener () {
 
 function resetClickHandler () {
     console.log('reset!');
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            board[i][j] = EMPTY;
+            renderSymbolInCell(EMPTY, i, j);
+        }
+    }
+    turn = CROSS;
+    victory = false;
 }
 
 
